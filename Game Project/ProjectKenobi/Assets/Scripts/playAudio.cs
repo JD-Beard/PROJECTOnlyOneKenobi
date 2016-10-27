b86b3264cbd8343f1768ad9ada74bcf197eecbc3
DESKTOP-DPSQ9CB\tanis <tanishkarmakar@gmail.com>
2016-10-27 14:39:35 -0400

    Completed the main menu and started work on other scenes

---
+++ Game Project/ProjectKenobi/Assets/Scripts/playAudio.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playAudio : MonoBehaviour
+{
+
+    void OnTriggerEnter()
+    {
+        GetComponent<AudioSource>().Play();
+    }
+    void OnTriggerExit()
+    {
+        GetComponent<AudioSource>().Stop();
+    }
+}
